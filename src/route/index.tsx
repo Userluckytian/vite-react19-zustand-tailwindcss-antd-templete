47f4d33dbd0a74c160b9f48b079fdd02fe497fc5
tianzhuzhu <tianlukang123@163.com>
2025-07-02 09:52:17 +0800

    🚨 fix：修复打包时，页面展示异常的bug（从broswer更换为hash路由的方式 + 增加了打印sourceMap数据以定位具体错误位置）

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,21 +1,22 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter, Navigate } from "react-router";
 
 
 import { lazy } from "react";
 import LazyLoadComp from "./lazyLoadComp";
-import RouteDemo from "@/pages/route-demo";
 import { sleep } from "@/utils/utils";
 
-
-const router = createBrowserRouter(
+const router = createHashRouter(
     [
+        // path为空时，重定向到layout
+        { path: '', element: <Navigate to="/layout" /> },
+        // 使用LazyLoadComp动态加载页面
         {
-            path: '',
+            path: '/layout',
             element: LazyLoadComp(lazy(() => import("@/layout"))),
             children: [
                 { index: true, element: LazyLoadComp(lazy(() => import("@/pages/home"))) },
                 {
-                    path: "/teams/:teamId",
+                    path: "teams/:teamId", // 这里要么写的时候不带/，如果带了/，就要把/layout也带上，即写完整
                     action: async ({ params }) => {
                         await sleep(3000);
                         let team = await Promise.resolve({ age: 18, name: 'xiao', teamId: params.teamId });
@@ -30,10 +31,14 @@ const router = createBrowserRouter(
                     //     return { age: 18, name: 'xiao ming', teamId: params.teamId };
                     // },
                     // Component: RouteDemo,
-                    element: LazyLoadComp(lazy(()=>import('@/pages/route-demo')))
+                    element: LazyLoadComp(lazy(() => import('@/pages/route-demo')))
                 },
                 { path: "404", element: LazyLoadComp(lazy(() => import("@/components/NotFoundPage"))) }
             ]
+        },
+        {
+            path: "/*",
+            element: LazyLoadComp(lazy(() => import("@/components/NotFoundPage"))),
         }
     ]
 );
