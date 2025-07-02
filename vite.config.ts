b9230f66f08ec14e9800189fc094cdaa0b7e9870
tianzhuzhu <tianlukang123@163.com>
2025-07-02 13:04:16 +0800

    🚧 fix：添加打包配置项

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,54 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
+
+/**
+ * JS模块分包
+ * @param id - 标识符
+ */
+function splitJSModules(id: string) {
+  // pnpm兼容
+  const pnpmName = id.includes('.pnpm') ? '.pnpm/' : '';
+  const fileName = `node_modules/${pnpmName}`;
+
+  const result = id
+    .split(fileName)[1]
+    .split('/')[0]
+    .toString();
+
+  return result;
+}
+
+
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
   build: {
-    sourcemap: true,
+    chunkSizeWarningLimit: 1000, // 大于1000k才警告
+    sourcemap: process.env.NODE_ENV !== 'production', // 非生产环境开启
+    minify: true,
+    terserOptions: {
+      compress: {
+        // 生产环境时移除console和debugger
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name].[hash].js',
+        entryFileNames: 'assets/js/[name].[hash].js',
+        assetFileNames: 'assets/[ext]/[name].[hash].[ext]',
+        manualChunks(id) {
+          // JS模块
+          if (id.includes('node_modules')) {
+            return splitJSModules(id);
+          }
+        }
+      }
+    }
   },
-  resolve:{
+  resolve: {
     alias: {
       '@': '/src',
     },
